b77f3d07a566bd338201ea864abd5d9bbd867174
arifalvi800 <arifalvi800@gmail.com>
2024-09-14 19:16:41 +0530

    Fitness Elite

--- time_table/models.py
+++ time_table/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Table(models.Model):
     day=models.CharField(max_length=15, default="")
-    timing=models.TimeField(max_length=15, default="")
-    duration=models.IntegerField(max_length=10,default="")
-    room_no=models.IntegerField(max_length=10,default="")
+    timing=models.TimeField()
+    duration=models.IntegerField()
+    room_no=models.IntegerField()
     
